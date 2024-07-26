e08d983ca84a6c8453fb9c4ab8456cc041d5a769
Hannes <83277529+Hannes-Schniz@users.noreply.github.com>
2024-07-26 21:33:09 +0000

    done with 3 diff algorithms

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
-
+import random
+import sys
 
 Apex_Legends_Characters = sorted(['Ash','Fuse','Mad Maggie','Bangalore','Ballistic','Alter','Wraith','Mirage','Octane','Horizon','Valkyrie','Pathfinder','Revenant','Bloodhound','Crypto','Seer','Vantage','Caustic','Wattson','Rampart','Catalyst','Conduit','Lifeline','Loba','Gibraltar','Newcastle'])
 
@@ -45,31 +45,86 @@ def makeDict(challenges: list):
 def getCharacterChalSize(charDict:dict):
     amounts = []
     for key in charDict.keys():
-        amounts.append((Apex_Legends_Characters[key], len(charDict.get(key))))
+        amounts.append((key, len(charDict.get(key))))
     return amounts
 
 
-def printAmount(amontList):
+def printList(amontList):
     for amount in amontList:
         print(amount)
 
-#challenges = getChallenges()
-#
-#graph = constructCharacterGraph([x for x in challenges])
-#
-#print(challenges)
-#
-#
-#printCharacters(challenges,graph)
-#     
+
+def updateDict(charDict:dict, key):
+    values = charDict.pop(key)
+    for key in charDict.keys():
+        charDict[key] = [x for x in charDict.get(key) if x not in values]
+
+
+def buildGreedyPath(charDict:dict, path:list):
+    if len(charDict.keys()) == 0:
+        return path
+    sortedAmounts = sorted(getCharacterChalSize(charDict), reverse=True, key=lambda x: x[1])
+    path.append(Apex_Legends_Characters[sortedAmounts[0][0]])
+    updateDict(charDict, sortedAmounts[0][0])
+    return buildGreedyPath(charDict, path)        
+
+def getGreedyPath(charDict:dict):
+    greedyPath = buildGreedyPath(charDict.copy(), [])
+    print(len(greedyPath))
+    printList(greedyPath)
+
+def buildLessGreedyPath(charDict:dict, path:list):
+    if len(charDict.keys()) == 0:
+        return path
+    charChalSizes= sorted(getCharacterChalSize(charDict), reverse=True, key=lambda x: x[1])
+    maxAmounts = []
+    for character in [x[0] for x in charChalSizes if x[1] is charChalSizes[0][1]]:
+        values = charDict.get(character)
+        currAmount = [len(x) for x in values]
+        maxAmounts.append((character, currAmount))
+    
+    maxAmounts= sorted(maxAmounts, reverse=True, key=lambda x: x[1])
+    path.append(Apex_Legends_Characters[maxAmounts[0][0]])
+    updateDict(charDict, maxAmounts[0][0])
+    return buildLessGreedyPath(charDict, path)
+
+def getLessGreedyPath(charDict:dict):
+    greedyPath = buildLessGreedyPath(charDict.copy(), [])
+    print(len(greedyPath))
+    printList(greedyPath)
+    
+
+def buildRandomPath(charDict:dict, path:list):   
+    if len(charDict.keys()) == 0:
+        return path
+    key = random.choice([x for x in charDict.keys()])
+    path.append(Apex_Legends_Characters[key])
+    updateDict(charDict, key)
+    return buildRandomPath(charDict, path)
+
+def getRandomPath(charDict:dict, runs:int):
+    bestPath= buildRandomPath(charDict.copy(), [])
+    for i in range(runs):
+        newPath = buildRandomPath(charDict.copy(), [])
+        print('Calculating best Route: ' + str(round(100 / (runs -1) * i, 1)) +'%', end='\r', flush=True)
+        if len(newPath) < len(bestPath):
+            bestPath = newPath
+    print()
+    print(len(bestPath))
+    printList(bestPath)
 
 # A Dictionary of characters that shows which challenges get completed if a character is played
 challenge_Dictionary = makeDict(testChallenges)
 
-#for key in sorted(challenge_Dictionary.keys()):
-#    print(Apex_Legends_Characters[key], [[Apex_Legends_Characters[y] for y in x] for x in challenge_Dictionary.get(key)])
+print()
+print("Greedy Path:")
+getGreedyPath(challenge_Dictionary)
 
-sortedAmounts = sorted(getCharacterChalSize(challenge_Dictionary), reverse=True, key=lambda x: x[1])
+print()
+print("Double weighted path:")
+getLessGreedyPath(challenge_Dictionary)
 
-printAmount(sortedAmounts)
-#printAmount(getCharacterChalSize(challenge_Dictionary))
+revisions = 100000000
+print()
+print("Random path with", revisions, "revision:")
+getRandomPath(challenge_Dictionary, revisions)
